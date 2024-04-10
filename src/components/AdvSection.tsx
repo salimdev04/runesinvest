16709c65524e3dc8f6b96fe405e26f25aba860ad
salime soulemane <salimesoulemane@salimes-MacBook-Pro.local>
2024-04-10 15:54:05 +0000

    first comminvvv

--- src/components/AdvSection.tsx
+++ src/components/AdvSection.tsx
@@ -32,9 +32,9 @@ export default function AdvSection() {
                 <h1 className={h1Title}>Bringing Mass Adoption to Runes Ecosystem with our Multichain Runes Keys NFT Collection</h1>
 
                 <div className="flex justify-around gap-6 py-[4.5rem]">
-                    <IconCard title="Open Mint" content="Mint is open from 10 April to 17 AprilThere is no wallet limits for Keys you can Forge.Mint price is 0,0014 BTC" image="/icon/btc.svg" />
-                    <IconCard title="Open Mint" content="Our NFT Collection brings you a curated selection of rare and secure digital assets. Explore the possibilities and seize the opportunity to grow your portfolio." image="/icon/eth.svg" />
-                    <IconCard title="Coming Soon" content="Our NFT Collection offers the potential for high returns. Don't miss out on this exciting opportunity to invest in the future of blockchain technology." image="/icon/sol.svg" />
+                    <IconCard title="Open Mint" content="Mint is open from 10 April to 17 AprilThere is no wallet limits for Keys you can Forge.Mint price is 0,0014 BTC" image="/Icon/btc.svg" />
+                    <IconCard title="Open Mint" content="Our NFT Collection brings you a curated selection of rare and secure digital assets. Explore the possibilities and seize the opportunity to grow your portfolio." image="/Icon/eth.svg" />
+                    <IconCard title="Coming Soon" content="Our NFT Collection offers the potential for high returns. Don't miss out on this exciting opportunity to invest in the future of blockchain technology." image="/Icon/sol.svg" />
                 </div>
             </section>
 
