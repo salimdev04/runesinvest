b80c38543e47cf13c3b672711fb0793f9f226f66
salime soulemane <salimesoulemane@salimes-MacBook-Pro.local>
2024-04-14 20:54:25 +0100

    small finished.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,32 +12,32 @@ import { useState } from "react";
 export default function Navbar() {
     const [toggleMenu, setToggleMenu] = useState(false);
 
-    const MobileNav = () => {
-        return (
-            <div
-                className={`absolute top-0 left-0 h-screen p-[2rem] z-10 w-screen bg-[#fff] flex flex-col justify-between items-start transform ${toggleMenu ? "-translate-x-0" : "-translate-x-full"
-                    } transition-transform duration-300 ease-in-out filter  `}
-            >
-                <div className=" w-full">
-                    <div
-                        className="flex justify-between cursor-pointer mb-[2rem]"
-                        onClick={() => setToggleMenu(!toggleMenu)}
-                    >
-                        <h1 className=" font-bold">Logo</h1>
-                        <img src="/close.png" width={30} alt="" />
-                    </div>
-                    <nav className=" flex flex-col ">
-                        <Link href={"#"} className="py-2">Invest Now</Link>
-                        <Link href={"#"} className="py-2">About Us</Link>
-                        <Link href={"#"} className="py-2">Contact Us</Link>
-                        <Link href={"#"} className="py-2">More</Link>
-                    </nav>
+    // const MobileNav = () => {
+    //     return (
+    //         <div
+    //             className={`absolute top-0 left-0 h-screen p-[2rem] z-10 w-screen bg-[#fff] flex flex-col justify-between items-start transform ${toggleMenu ? "-translate-x-0" : "-translate-x-full"
+    //                 } transition-transform duration-300 ease-in-out filter  `}
+    //         >
+    //             <div className=" w-full">
+    //                 <div
+    //                     className="flex justify-between cursor-pointer mb-[2rem]"
+    //                     onClick={() => setToggleMenu(!toggleMenu)}
+    //                 >
+    //                     <h1 className=" font-bold">Logo</h1>
+    //                     <img src="/close.png" width={30} alt="" />
+    //                 </div>
+    //                 <nav className=" flex flex-col ">
+    //                     <Link href={"#"} className="py-2">Invest Now</Link>
+    //                     <Link href={"#"} className="py-2">About Us</Link>
+    //                     <Link href={"#"} className="py-2">Contact Us</Link>
+    //                     <Link href={"#"} className="py-2">More</Link>
+    //                 </nav>
 
-                </div>
+    //             </div>
 
-            </div>
-        );
-    }
+    //         </div>
+    //     );
+    // }
 
     return (
         <header className={styles.header}>
