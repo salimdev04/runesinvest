07de5e712d8745f16352666d82172d238086e8c2
salime soulemane <salimesoulemane@salimes-MacBook-Pro.local>
2024-04-14 22:04:43 +0100

    small finished.

--- src/components/EarlySection.tsx
+++ src/components/EarlySection.tsx
@@ -6,7 +6,14 @@ import { EarlyCard } from "./CommonUI"
 export default function EarlySection() {
     return (
         <section className={[commonStyles.padding, ""].join(' ')} >
+
             <div className={["tablet:flex justify-around pb-8 items-center"].join(' ')}>
+                <div className="mt-6 ">
+                    <img src="/runes-side.png" alt="" className=" tablet:max-w-[616px]" />
+                    <div className="flex justify-center mt-6">
+                        <a href="https://twitter.com/RunesAcademy" target="_blank" ><button className=" py-3 bg-white text-black rounded-[20px] w-[151px] font-bold">Join Us</button></a>
+                    </div>
+                </div>
                 <div >
                     <h1 className={[h1Title, 'text-[40px]'].join(' ')}> Being Early Matters <br />Open the Runes Gate with <br />your Forged Keys</h1>
                     <p className={[commonStyles.lightText, "py-6 text-lg max-w-[616px]"].join(" ")}>Each Key is backed with Runes Tokens that our team will mint. <br />Our Asset Managers and Devs will select the best tokens to mint early based on Market Sentiment and Alpha calls. <br />Invest 100$ and let us expose you to this new market opportunity.</p>
@@ -20,9 +27,7 @@ export default function EarlySection() {
                         </div>
                     </div>
                 </div>
-                <div className="mt-6">
-                    <img src="/runes-side.png" alt="" className=" tablet:max-w-[616px]" />
-                </div>
+
             </div>
         </section>
     )
